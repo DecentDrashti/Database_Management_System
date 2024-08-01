358ba53e1cba84fd6494e67dfc00389bde8a629a
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2024-08-01 13:20:18 +0530

    lab-6

---
+++ SQLQuery6(part-1).sql
@@ -0,0 +1,55 @@
+--Lab-6
+
+--MATH FUNCTION
+--Part – A:
+--1. Display the result of 5 multiply by 30.
+SELECT 5*30 AS RESULT
+
+--2. Find out the absolute value of -25, 25, -50 and 50.
+SELECT ABS(25), ABS(-50),ABS(-25), ABS(50)
+
+--3. Find smallest integer value that is greater than or equal to 25.2, 25.7 and -25.2.
+SELECT CEILING(25.2), CEILING(25.7),CEILING(-25.2)
+
+--4. Find largest integer value that is smaller than or equal to 25.2, 25.7 and -25.2.
+SELECT FLOOR(25.2), FLOOR(25.7), FLOOR(-25.2)
+--5. Find out remainder of 5 divided 2 and 5 divided by 3.
+SELECT 5%2,5%3 AS RESULT
+
+--6. Find out value of 3 raised to 2nd power and 4 raised 3rd power.
+SELECT POWER(3, 2),POWER(4,3)
+--7. Find out the square root of 25, 30 and 50.
+SELECT SQRT(25),SQRT(30),SQRT(50)
+--8. Find out the square of 5, 15, and 25
+SELECT SQUARE(5),SQUARE(15),SQUARE(25)--9. Find out the value of PI.
+SELECT PI()
+
+--10. Find out round value of 157.732 for 2, 0 and -2 decimal points.
+SELECT ROUND(157.732, 2),ROUND(157.732, 0),ROUND(157.732, -2)
+
+--11. Find out exponential value of 2 and 3.
+SELECT EXP(2),EXP(3)
+
+--12. Find out logarithm having base e of 10 and 2.
+SELECT LOG(10),LOG(2)
+
+--13. Find out logarithm having base b having value 10 of 5 and 100.
+SELECT LOG10(5),LOG10(100)
+
+--14. Find sine, cosine and tangent of 3.1415.
+SELECT SIN(3.1415),COS(3.1415),TAN(3.1415)
+
+--15. Find sign of -25, 0 and 25.
+SELECT SIGN(-25), SIGN(0), SIGN(25)
+
+--16. Generate random number using function.SELECT RAND()*23--Part – B:CREATE TABLE EMP_MASTER();--1. Display the result of Salary plus Commission.
+
+--2. Find smallest integer value that is greater than or equal to 55.2, 35.7 and -55.2.
+--3. Find largest integer value that is smaller than or equal to 55.2, 35.7 and -55.2.
+--4. Find out remainder of 55 divided 2 and 55 divided by 3.
+--5. Find out value of 23 raised to 2nd power and 14 raised 3rd power.--Part – C:
+--1. Find out the square root of 36, 49 and 81.
+--2. Find out the square of 3, 9, and 12.
+--3. Find out round value of 280.8952 for 2, 0 and -2 decimal points.
+--4. Find sine, cosine and tangent of 4.2014.
+--5. Find sign of -55, 0 and 95.
---
+++ SQLQuery6(part-2).sql
@@ -0,0 +1,70 @@
+--Lab-6
+
+--STRING FUNCTION
+--Part – A:
+--1. Find the length of following. (I) NULL (II) ‘ hello ’ (III) Blank
+Select LEN('NULL'),LEN(' hello '),LEN('Blank')
+
+--2. Display your name in lower & upper case.
+Select LOWER('DRASHTI'), UPPER('Drashti')
+
+--3. Display first three characters of your name.
+SELECT LEFT('DRASHTI',3)
+
+--4. Display 3rd to 10th character of your name.
+SELECT SUBSTRING('Drashti Ruparelia', 3, 8)
+
+--5. Write a query to convert ‘abc123efg’ to ‘abcXYZefg’ & ‘abcabcabc’ to ‘ab5ab5ab5’ using REPLACE.
+SELECT REPLACE('abc123efg','abc123efg','abcXYZefg')
+
+--6. Write a query to display ASCII code for ‘a’,’A’,’z’,’Z’, 0, 9.
+SELECT ASCII('A'), ASCII('a'),ASCII('Z'), ASCII('z'),ASCII('0'),ASCII('9')
+--7. Write a query to display character based on number 97, 65,122,90,48,57.
+SELECT CHAR('97'),CHAR('65'),CHAR('122'),CHAR('90'),CHAR('48'),CHAR('57')
+
+--8. Write a query to remove spaces from left of a given string ‘hello world ‘.
+SELECT LTRIM(' HELLO WORLD')
+
+--9. Write a query to remove spaces from right of a given string ‘ hello world ‘.
+SELECT RTRIM('HELLO WORLD ')
+--10. Write a query to display first 4 & Last 5 characters of ‘SQL Server’.
+SELECT LEFT('SQL Server',4),LEFT('SQL Server',5)
+
+--11. Write a query to convert a string ‘1234.56’ to number (Use cast and convert function).
+SELECT CONVERT(int,1234.56)
+
+--12. Write a query to convert a float 10.58 to integer (Use cast and convert function).
+SELECT CONVERT(FLOAT,10.58)
+
+--13. Put 10 space before your name using function.
+SELECT (SPACE(10)+'DARSHAN UNIVERSITY')
+
+--14. Combine two strings using + sign as well as CONCAT ().
+SELECT CONCAT('Darshan','-','University')
+SELECT 'Darshan'+'University'
+
+--15. Find reverse of “Darshan”.
+SELECT REVERSE('Darshan')
+
+--16. Repeat your name 3 times.
+SELECT REPLICATE('Darshan',3)
+
+
+
+--Part – B: Perform following queries on Student table of practical no 5.
+--1. Find the length of FirstName and LastName columns.
+--2. Display FirstName and LastName columns in lower & upper case.
+--3. Display first three characters of FirstName column.
+--4. Display 3rd to 10th character of Website column.
+--5. Write a query to display first 4 & Last 5 characters of Website column.
+
+
+--Part – C: Perform following queries on Student table of practical no 5.
+--1. Put 10 space before FirstName using function.
+--2. Combine FirstName and LastName columns using + sign as well as CONCAT ().
+--3. Combine all columns using + sign as well as CONCAT ().
+--4. Find reverse of FirstName column.
+--5. Repeat FirstName column 3 times
+--6. Give the Names which contains 5 characters.
+--7. Combine the result as <FirstName> Lives in <City>.
+--8. Combine the result as Student_ID of < FirstName > is <StuID> .
